e705ef2126033d4628e4947664ff0bfd918cf6d0
숩 <realhsb@gmail.com>
2025-04-23 19:48:25 +0900

    chore: 안 쓰는 주석 및 파일 삭제

--- FourStars/FourStars/Source/Model/DailyStamp/DailyStamp.swift
+++ FourStars/FourStars/Source/Model/DailyStamp/DailyStamp.swift
@@ -12,11 +12,6 @@ import SwiftData
 class DailyStamp {
     var date: Date
 
-//    @Relationship var morningResult: HabitResult
-//    @Relationship var afternoonResult: HabitResult
-//    @Relationship var eveningResult: HabitResult
-//    @Relationship var extraResult: HabitResult
-    
     @Relationship var results: [HabitResult] = []
     
     var habitResults: [HabitResult] {
@@ -27,10 +22,6 @@ class DailyStamp {
 
     init(date: Date) {
         self.date = date.strippedTime // 시분초 00으로 설정
-//        self.morningResult = HabitResult(timeSlot: .morning, status: .notDone)
-//        self.afternoonResult = HabitResult(timeSlot: .afternoon, status: .notDone)
-//        self.eveningResult = HabitResult(timeSlot: .evening, status: .notDone)
-//        self.extraResult = HabitResult(timeSlot: .extra, status: .notDone)
     }
 }
 
